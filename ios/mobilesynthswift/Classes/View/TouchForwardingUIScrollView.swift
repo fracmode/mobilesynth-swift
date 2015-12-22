a7b29f7a0936a7e3f8137555f1f288840bdd03c1
fracmode <amunu.rets@gmail.com>
2015-12-22 15:50:34 +0900

    [add] add TouchForwardingUIScrollView
    add TouchForwardingUIScrollView

---
+++ ios/mobilesynthswift/Classes/View/TouchForwardingUIScrollView.swift
@@ -0,0 +1,14 @@
+//
+//  TouchForwardingUIScrollView.swift
+//  mobilesynthswift
+//
+//  Created by Sho Terunuma on 22/12/2015.
+//  Copyright © 2015 TestOrg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TouchForwardingUIScrollView : UIScrollView {
+    
+}
