6017347411c0aefd728291cae19b0b29ded47573
fracmode <amunu.rets@gmail.com>
2015-12-24 23:34:49 +0900

    [add] add TouchForwardingUIScrollView (2)
    add TouchForwardingUIScrollView (2)

--- ios/mobilesynthswift/Classes/View/TouchForwardingUIScrollView.swift
+++ ios/mobilesynthswift/Classes/View/TouchForwardingUIScrollView.swift
@@ -9,6 +9,29 @@
 import Foundation
 import UIKit
 
-class TouchForwardingUIScrollView : UIScrollView {
+class TouchForwardingUIScrollView: UIScrollView {
+    var touchView: UIView? = nil
+
+    // touchesEnded
+    func _touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        super.touchesEnded(touches as! Set<UITouch>, withEvent: event)
+        let subview:UIView? = self.hitSubview(touches, withEvent: event)
+        if ( subview != nil ) {
+            self.touchView!.touchesEnded(touches as! Set<UITouch>, withEvent: event)
+        }
+    }
     
+    // hitSubview
+    func hitSubview(touches: NSSet, withEvent event: UIEvent) -> UIView? {
+        let touchArray = touches.allObjects as NSArray
+        for var i = 0; i < touchArray.count; ++i {
+            let touch = touchArray.objectAtIndex(i)
+            let point = touch.locationInView(self)
+            let subview:UIView? = self.hitTest(point, withEvent: event)
+            if ( subview != nil ) {
+                return subview
+            }
+        }
+        return nil
+    }
 }
